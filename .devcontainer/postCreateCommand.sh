20cc26a6bc54e5ddc0dfcfdc230b599de3a70d0f
David Toneian <david@toneian.com>
2023-02-16 00:11:00 +0100

    Avoid uninstalling python packages by installing pinned versions first.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,12 +1,15 @@
 #! /usr/bin/env sh
 
 ./install.sh
-pip install -r requirements.txt
 
 # Install optional dependencies:
+# Do this before installing `requirements.txt` below, the optional dependencies
+# have fixed versions, while `requirements.txt` only has minimum versions.
 . open_spiel/scripts/python_extra_deps.sh
 pip install \
     $OPEN_SPIEL_PYTHON_JAX_DEPS \
     $OPEN_SPIEL_PYTHON_PYTORCH_DEPS \
     $OPEN_SPIEL_PYTHON_TENSORFLOW_DEPS \
     $OPEN_SPIEL_PYTHON_MISC_DEPS
+
+pip install -r requirements.txt
