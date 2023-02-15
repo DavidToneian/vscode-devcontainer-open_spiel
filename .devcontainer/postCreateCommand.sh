2a1fb9825f2099886752e45d6edadac41a61725d
David Toneian <david@toneian.com>
2023-02-15 22:58:48 +0100

    Install optional dependencies.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,3 +2,11 @@
 
 ./install.sh
 pip install -r requirements.txt
+
+# Install optional dependencies:
+. open_spiel/scripts/python_extra_deps.sh
+pip install \
+    $OPEN_SPIEL_PYTHON_JAX_DEPS \
+    $OPEN_SPIEL_PYTHON_PYTORCH_DEPS \
+    $OPEN_SPIEL_PYTHON_TENSORFLOW_DEPS \
+    $OPEN_SPIEL_PYTHON_MISC_DEPS
